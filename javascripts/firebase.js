1f1e8bcd34e121b4c2d2f7462c327098a3ac0dd2
Erling Holte <85932175+erlhol@users.noreply.github.com>
2023-05-19 10:38:00 +0000

    Test API-key

--- javascripts/firebase.js
+++ javascripts/firebase.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-import { apiKey } from './config.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: apiKey,
+  apiKey: "AIzaSyCkeH9dZ53yTDaUlpLR0kPBSp-NUjbMwsU",
   authDomain: "uio-project.firebaseapp.com",
   projectId: "uio-project",
   storageBucket: "uio-project.appspot.com",
