84544da10be3e5a1f2191c496f23f665f94f099e
Erling Holte <85932175+erlhol@users.noreply.github.com>
2023-05-24 10:05:19 +0000

    Restart project

--- javascripts/main.js
+++
@@ -1,5 +0,0 @@
-console.log('This would be the main JS file.');
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Teksten ble endret";
-  }
---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkeH9dZ53yTDaUlpLR0kPBSp-NUjbMwsU",
+  authDomain: "uio-project.firebaseapp.com",
+  projectId: "uio-project",
+  storageBucket: "uio-project.appspot.com",
+  messagingSenderId: "1055590953157",
+  appId: "1:1055590953157:web:6ab0bb93573bf2c31780ac",
+  measurementId: "G-BZ0XP6WG0P"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+/*
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    auth.EmailAuthProvider.PROVIDER_ID,
+    auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  // Other config options...
+});
+*/
